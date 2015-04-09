ba97c61b597a3805ff1180c1ac00e296fc77880b
Tony <coolhzb@163.com>
2015-04-10 00:49:58 +0800

    fix the not found issue...

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 	"path"
 	"fmt"
+	"os"
 	
 	"github.com/AdoHe/etcd-dashboard/third_party/github.com/gorilla/mux"
 )
+
+var pwd string
 
 func addSlash(w http.ResponseWriter, req *http.Request) {
 	http.Redirect(w, req, path.Join("mod", req.URL.Path)+"/", 302)
@@ -15,15 +18,16 @@ func addSlash(w http.ResponseWriter, req *http.Request) {
 
 func indexPage(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s", "serve index page")
-	http.ServeFile(w, req, path.Join("../dashboard/dist", "index.html"))
+	http.ServeFile(w, req, path.Join(pwd, "/dashboard/dist", "index.html"))
 }
 
 func HttpHandler() (handler http.Handler) {
-	handler = http.FileServer(http.Dir("../dashboard/dist"))
+	handler = http.FileServer(http.Dir(path.Join(pwd, "/dashboard/dist")))
 	return handler
 }
 
 func HTTPHandler() http.Handler {
+	pwd, _ = os.Getwd()
 	r := mux.NewRouter()
 	r.HandleFunc("/dashboard", addSlash)
 
